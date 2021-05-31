e5776384136b72f38706ddeef5240f4481cd8da5
sarrio <samarixyt@gmail.com>
2021-05-30 22:14:43 -0400

    Added comments

--- PosAI/Core.cpp
+++ PosAI/Core.cpp
@@ -2,12 +2,14 @@
 
 std::string totalData[2000];
 
+// Reads and prepares given input.
 void Read::Input(std::string _stringInput) {
     //TODO: Some error checking with the string.
     Seek classObject;
     classObject.collectData(_stringInput);
 }
 
+// Makes room for new spot, then fills the spot with requested data.
 void Seek::collectData(std::string newData) {
     int totalItems = sizeof(totalData) / sizeof(totalData[0]);
     for (size_t i = 0; i < totalItems; i++)
