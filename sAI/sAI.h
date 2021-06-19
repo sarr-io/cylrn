97476a182df3933263403f5c3f6342e708cd47c3
sarrio <samarixyt@gmail.com>
2021-06-18 21:53:00 -0400

    Added more detail to comments regarding the exp.

--- sAI/sAI.h
+++ sAI/sAI.h
@@ -18,23 +18,24 @@ class sAI {
     private:
         float activation(int n, int _mode) {
             switch (_mode) {
+                float result;
                 // Will be PReLU in the future.
                 case 0:
                     // Leaky Rectified Linear Unit
-                    float lreluResult;
+                    
                     if (n < 0) {
-                        lreluResult = 0.01 * n;
+                        result = 0.01 * n;
                     }
-                    return lreluResult;
+                    return result;
                 case 1:
                     // Sigmoid
-                    float sigResult = 1.0 / 1.0 + custom_exp(-n);
-                    return sigResult;
+                    float result = 1.0 / 1.0 + custom_exp(-n);
+                    return result;
             }
         }
 
         // WILL NOT WORK, NEEDS CHANGE!
-        // FOR LOOP DOES NOT PROPPERLY WORK WITH FLOATER POINT VALUES.
+        // FOR LOOP DOES NOT PROPPERLY WORK WITH FLOATER POINT VALUES (2.71828 is not divisible by n+1).
         float custom_exp(float _n) {
             float e = 2.71828;
             for (int i = 0; i < _n; i++) {
