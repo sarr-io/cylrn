e3ac07ddaa62a8a7886cdb87d1a1701e5085e58e
sarrio <samarixyt@gmail.com>
2021-07-18 15:19:47 -0400

    Fixed/finalized temporary version of custom psuedo-random function

--- cylrn/cylrn.h
+++ cylrn/cylrn.h
@@ -14,20 +14,17 @@ class cylrn {
             layer_count = sizeof(layer_sizes)/sizeof(layer_sizes[0]);
             l_sizes = layer_sizes;
 
-            if (layer_count > 0) {
-                // Filling each layer with pseudo-random numbers to start off with, using for loop.
-                for (int i = 0; i < layer_count; i++)
-                {
-                    for (size_t i = 0; i < sizeof(l_sizes)/sizeof(l_sizes[0]); i++)
-                    {
-                        // TODO: Custom random number algorith, then mod() it down to 0-1.
-                        weights[i] = 0;
-                        biases[i] = 0;
-                    }
+            // Filling each layer with pseudo-random numbers to start off with, using for loop.
+            for (int i = 0; i < layer_count; i++)
+            {
+                for (size_t i = 0; i < sizeof(l_sizes)/sizeof(l_sizes[0]); i++)
+                {   
+                    // Just to make people mad
+                    char *w_addr = (char*)&weights;
+                    char *b_addr = (char*)&biases;
+                    weights[i] = pseudo_ran(w_addr, i);
+                    biases[i] = pseudo_ran(b_addr, i);
                 }
-            }
-            else { // If layer_sizes is 0 it will automatically choose a recommended amount of layers based on input training size.
-
             }
         }
 
@@ -40,10 +37,10 @@ class cylrn {
         void Push(int _newInput[], char _loadModelPath[], int _outputSettings[]) {
 
         }
-    private:
-        double evaluate(int _data) {} // TLDR; Evaluates the data and finds the neuron with the highest activation.
 
-        double activation_feed() {} // TLDR; Returns the dot product/scalar of the current weights and number (usually the index of a neuron) in a bias,weight for loop.
+    private:
+
+        double evaluate(int _data) {} // TLDR; Evaluates the data and finds the neuron with the highest activation.
 
         double backprop(double _x[], double _y) {} // TLDR; Returns a tuple representing the gradient for the cost function. (Holds a list of activations from _x[])
 
@@ -67,6 +64,30 @@ class cylrn {
                 //     float result = 1.0 / 1.0 + custom_exp(-n);
                 //     return result;
             }
+        }
+
+        // Will more than likely replace in the future.
+        double pseudo_ran(char var_addr[], int n) {
+            for (int i = 2; i < sizeof(var_addr)/sizeof(var_addr[0]); i++)
+            {
+                try
+                {
+                    int seed = (int)var_addr[i] * n;
+                    // Random system of equations that generates a random-looking number.
+                    double num = ((seed / 100 * 345287 + 12 / 1200) / 0.4233 + 1) - 2 % 1;
+                    if(num > 1) {
+                        return 1;
+                    }
+                    else {
+                        return num;
+                    }
+                }
+                catch(int filler)
+                {
+                    // Nothing
+                }
+            }
+            
         }
         
         // WILL NOT WORK, NEEDS CHANGE!
